5637cd634f2206cb7e32b320151610706f2424c9
Joshuawilfred <joshuakaaya216@gmail.com>
2026-04-24 20:19:06 +0300

    Basic plain app w/logo

---
+++ resources/js/components/app-logo-icon.tsx
@@ -0,0 +1,8 @@
+import type { SVGAttributes } from 'react';
+
+export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
+    return (
+       <img src="/images/logo.png" alt="SmartRental Logo" {...props} />
+    );
+}
+// y3BUnjyN@kpWY@B 
