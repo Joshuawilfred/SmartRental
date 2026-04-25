ef6d67170c3e9559c96e6e63af2a5edbae493085
Joshuawilfred <joshuakaaya216@gmail.com>
2026-04-25 04:38:02 +0300

    Push to deploy

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -3,8 +3,8 @@ import AppLogoIcon from '@/components/app-logo-icon';
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-white text-sidebar-primary-foreground">
+                <AppLogoIcon className="size-6 fill-current text-white dark:text-black" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="text-[18px] font-semibold tracking-tight text-[#F0F6F8]">
